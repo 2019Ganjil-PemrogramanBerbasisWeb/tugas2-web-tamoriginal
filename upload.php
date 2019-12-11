c1382d590d0285ee48a39f5e336f4b7bda48e94d
Anggada Putra <47882546+anggadaputra11319@users.noreply.github.com>
2019-12-11 12:29:17 +0700

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,59 +1,125 @@
 <?php
-
-include 'conn.php'; 
-
-
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-	$uploadOk = 1;
-}
-// Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
-    $uploadOk = 0;
-}
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 50000000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
-
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-		
-		
-		$sql = "INSERT INTO dbgambar (filepath) VALUES (?)";
-        if ($stmt = mysqli_prepare($link, $sql)) {
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_filepath);
-            // Set parameters
-            $param_filepath = 'uploads/' . basename( $_FILES["fileToUpload"]["name"]);
-            // Attempt to execute the prepared statement
-            if (mysqli_stmt_execute($stmt)) {
-                // Redirect to login page
-                header("location: index_upload.php");
-            } else {
-                echo "Something went wrong. Please try again later.";
-            }
-        }
-        // Close statement
-        mysqli_stmt_close($stmt);
-    
-		
-		
-		
-		
-    } else {
-        echo "Sorry, there was an error uploading your file.";
+$dir = '../uploads/';
+$msg = "";
+if(isset($_POST['submit'])){
+    $a="";
+    $b="";
+    $c="";
+    $d="";
+    $e="";
+    $f="";
+    $target_dir = "../uploads/";
+    $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+    if (file_exists($target_file)) {
+        $b = "Nama File Sudah Ada.<br>";
+        $uploadOk = 0;
     }
-}
+    if ($_FILES["fileToUpload"]["size"] > 500000) {
+        $c = "File Terlalu Besar.<br>";
+        $uploadOk = 0;
+    }
+    if ($uploadOk == 0) {
+        $e = "Tidak Bisa Upload.<br>";
+    } else {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            $f = "The file ". basename( $_FILES["fileToUpload"]["name"]). " telah di upload.";
+        } else {
+            $f = "Sorry, there was an error uploading your file.";
+        }
+    } $msg = $a . $b . $c . $d . $e . $f;
+    }
 ?>
+
+
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+
+<head>
+    <!-- Mobile Specific Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Favicon-->
+    <link rel="shortcut icon" href="img/fav.png">
+    <!-- Author Meta -->
+    <meta name="author" content="CodePixar">
+    <!-- Meta Description -->
+    <meta name="description" content="">
+    <!-- Meta Keyword -->
+    <meta name="keywords" content="">
+    <!-- meta character set -->
+    <meta charset="UTF-8">
+    <!-- Site Title -->
+    <title>Tamoriginal</title>
+
+    <!--
+            CSS
+            ============================================= -->
+    <link rel="stylesheet" href="css/linearicons.css">
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <link rel="stylesheet" href="css/nice-select.css">
+    <link rel="stylesheet" href="css/nouislider.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<html>
+<body>
+    <div class="container"><br>
+<?php if ($msg != "") { ?> <div class="alert alert-warning alert-dismissible fade show" role="alert">
+    <h4 class="alert-heading"><strong>Selamat!</strong></h4><hr>
+                <?php echo $msg;?>
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+                </div><?php }?>
+            
+                
+            </div>
+                
+<form action="Tanos.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+</form>
+<br><br><br><br>
+
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">No</th>
+      <th scope="col">Nama File</th>
+      <th scope="col">Download</th>
+    </tr>
+  </thead>
+  <tbody>
+      <?php                 
+        $dirr = 1;
+        foreach(glob($dir.'*',GLOB_BRACE) as $file){
+            $dirrr = basename($file);
+            echo '<tr><th>'. $dirr .
+            "</th><td>". $dirrr .  
+            "</td><td><a class='btn btn-primary' href='" . $file . "' role='button' download>Download</a></td></tr>"; $awek++;
+        }
+      ?>
+  </tbody>
+</table>
+
+<script src="js/vendor/jquery-2.2.4.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4"
+	 crossorigin="anonymous"></script>
+	<script src="js/vendor/bootstrap.min.js"></script>
+	<script src="js/jquery.ajaxchimp.min.js"></script>
+	<script src="js/jquery.nice-select.min.js"></script>
+	<script src="js/jquery.sticky.js"></script>
+    <script src="js/nouislider.min.js"></script>
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<!--gmaps Js-->
+	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjCGmQ0Uq4exrzdcL6rvxywDDOvfAu6eE"></script>
+	<script src="js/gmaps.min.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
